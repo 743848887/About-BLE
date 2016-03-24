b4fe519f8d699cfdfb0a56f730835658ee7c79ce
MacBook-pro <74384887@qq.com>
2016-03-24 21:16:26 +0800

    BLE

--- About-BLE/ViewController.m
+++ About-BLE/ViewController.m
@@ -7,16 +7,337 @@
 //
 
 #import "ViewController.h"
+#import <CoreBluetooth/CoreBluetooth.h>
+@interface ViewController () <CBCentralManagerDelegate,CBPeripheralDelegate>
 
-@interface ViewController ()
 
+/**
+ *  管理者
+ */
+@property (nonatomic ,strong)CBCentralManager *manager;
+/**
+ *  外接设备
+ */
+@property (nonatomic ,strong)CBPeripheral *peripheral;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.manager  =[[CBCentralManager alloc]initWithDelegate:self queue:nil options:nil];
+
+    
+
+}
+
+/**
+ *  扫描外接设备
+ *
+ */
+- (void)centralManagerDidUpdateState:(CBCentralManager *)central {
+    
+    switch (central.state) {
+        case CBCentralManagerStateUnknown:
+            NSLog(@">>>CBCentralManagerStateUnknown");
+            break;
+        case CBCentralManagerStateResetting:
+            NSLog(@">>>CBCentralManagerStateResetting");
+            break;
+        case CBCentralManagerStateUnsupported:
+            NSLog(@">>>CBCentralManagerStateUnsupported");
+            break;
+        case CBCentralManagerStateUnauthorized:
+            NSLog(@">>>CBCentralManagerStateUnauthorized");
+            break;
+        case CBCentralManagerStatePoweredOff:
+            NSLog(@">>>CBCentralManagerStatePoweredOff");
+            break;
+        case CBCentralManagerStatePoweredOn:
+            NSLog(@">>>CBCentralManagerStatePoweredOn");
+            //开始扫描周围的外设
+            /*
+             第一个参数nil就是扫描周围所有的外设，扫描到外设后会进入
+             - (void)centralManager:(CBCentralManager *)central didDiscoverPeripheral:(CBPeripheral *)peripheral advertisementData:(NSDictionary *)advertisementData RSSI:(NSNumber *)RSSI;
+             */
+            [self.manager scanForPeripheralsWithServices:nil options:nil];
+            
+            break;
+        default:
+            break;
+    }
+}
+
+/*
+ */
+- (void)centralManager:(CBCentralManager *)central willRestoreState:(NSDictionary<NSString *, id> *)dict {
+    
+}
+
+
+/**
+ *  发现外接设备
+ *
+ *  @param central           中心管理者
+ *  @param peripheral        外接设备
+ *  @param advertisementData 广告数据
+ *  @param RSSI              信号强度
+ */
+//  扫描－－断开－－连接
+- (void)centralManager:(CBCentralManager *)central didDiscoverPeripheral:(CBPeripheral *)peripheral advertisementData:(NSDictionary<NSString *, id> *)advertisementData RSSI:(NSNumber *)RSSI {
+ 
+    //接下连接我们的测试设备，如果你没有设备，可以下载一个app叫lightbule的app去模拟一个设备
+    //这里自己去设置下连接规则，我设置的是P开头的设备
+    
+    [self.manager stopScan];
+    
+    self.peripheral = peripheral;
+    
+    self.peripheral.delegate = self;
+    if ([peripheral.name hasPrefix:@"P"]){
+        /*
+         一个主设备最多能连7个外设，每个外设最多只能给一个主设备连接,连接成功，失败，断开会进入各自的委托
+         - (void)centralManager:(CBCentralManager *)central didConnectPeripheral:(CBPeripheral *)peripheral;//连接外设成功的委托
+         - (void)centralManager:(CBCentralManager *)central didFailToConnectPeripheral:(CBPeripheral *)peripheral error:(NSError *)error;//外设连接失败的委托
+         - (void)centralManager:(CBCentralManager *)central didDisconnectPeripheral:(CBPeripheral *)peripheral error:(NSError *)error;//断开外设的委托
+         */
+        //连接设备
+        [self.manager connectPeripheral:peripheral options:nil];
+    }
+    
+}
+
+/**
+ *  连接到Peripherals-成功
+ */
+- (void)centralManager:(CBCentralManager *)central didConnectPeripheral:(CBPeripheral *)peripheral {
+    
+    NSLog(@">>>连接到名称为（%@）的设备-成功",peripheral.name);
+    //设置的peripheral委托CBPeripheralDelegate
+    //@interface ViewController : UIViewController<cbcentralmanagerdelegate,cbperipheraldelegate>
+
+    self.peripheral = peripheral;
+    [peripheral setDelegate:self];
+    //扫描外设Services，成功后会进入方法：-(void)peripheral:(CBPeripheral *)peripheral didDiscoverServices:(NSError *)error{
+    [peripheral discoverServices:nil];
+    
+}
+
+/**
+ *  外接设备连击失败
+ *
+ */
+- (void)centralManager:(CBCentralManager *)central didFailToConnectPeripheral:(CBPeripheral *)peripheral error:(nullable NSError *)error {
+    
+}
+
+/**
+ *  外接设备断开连接
+ *
+ */
+- (void)centralManager:(CBCentralManager *)central didDisconnectPeripheral:(CBPeripheral *)peripheral error:(nullable NSError *)error {
+    
+}
+
+#pragma mark ----外接设备代理 CBPeripheralDelegate
+/**
+ *  更新外接设备
+ *
+ */
+- (void)peripheralDidUpdateName:(CBPeripheral *)peripheral NS_AVAILABLE(NA, 6_0) {
+    
+}
+
+/*!
+ *  @method peripheral:didModifyServices:
+ *
+ *  @param peripheral			The peripheral providing this update.
+ *  @param invalidatedServices	The services that have been invalidated
+ *
+ *  @discussion			This method is invoked when the @link services @/link of <i>peripheral</i> have been changed.
+ *						At this point, the designated <code>CBService</code> objects have been invalidated.
+ *						Services can be re-discovered via @link discoverServices: @/link.
+ */
+- (void)peripheral:(CBPeripheral *)peripheral didModifyServices:(NSArray<CBService *> *)invalidatedServices NS_AVAILABLE(NA, 7_0) {
+    
+}
+
+/*!
+ *  @method peripheralDidUpdateRSSI:error:
+ *
+ *  @param peripheral	The peripheral providing this update.
+ *	@param error		If an error occurred, the cause of the failure.
+ *
+ *  @discussion			This method returns the result of a @link readRSSI: @/link call.
+ *
+ *  @deprecated			Use {@link peripheral:didReadRSSI:error:} instead.
+ */
+- (void)peripheralDidUpdateRSSI:(CBPeripheral *)peripheral error:(nullable NSError *)error NS_DEPRECATED(NA, NA, 5_0, 8_0){
+    
+}
+
+/*!
+ *  @method peripheral:didReadRSSI:error:
+ *
+ *  @param peripheral	The peripheral providing this update.
+ *  @param RSSI			The current RSSI of the link.
+ *  @param error		If an error occurred, the cause of the failure.
+ *
+ *  @discussion			This method returns the result of a @link readRSSI: @/link call.
+ */
+- (void)peripheral:(CBPeripheral *)peripheral didReadRSSI:(NSNumber *)RSSI error:(nullable NSError *)error NS_AVAILABLE(NA, 8_0) {
+    
+}
+
+/*＊
+ *   扫描到外接设备
+ */
+- (void)peripheral:(CBPeripheral *)peripheral didDiscoverServices:(nullable NSError *)error {
+
+    //  NSLog(@">>>扫描到服务：%@",peripheral.services);
+    if (error)
+    {
+        NSLog(@">>>Discovered services for %@ with error: %@", peripheral.name, [error localizedDescription]);
+        return;
+    }
+    
+    for (CBService *service in peripheral.services) {
+        NSLog(@"%@",service.UUID);
+        //扫描每个service的Characteristics，扫描到后会进入方法： -(void)peripheral:(CBPeripheral *)peripheral didDiscoverCharacteristicsForService:(CBService *)service error:(NSError *)error
+        [peripheral discoverCharacteristics:nil forService:service];
+    }
+}
+
+/*!
+ *  @method peripheral:didDiscoverIncludedServicesForService:error:
+ *
+ *  @param peripheral	The peripheral providing this information.
+ *  @param service		The <code>CBService</code> object containing the included services.
+ *	@param error		If an error occurred, the cause of the failure.
+ *
+ *  @discussion			This method returns the result of a @link discoverIncludedServices:forService: @/link call. If the included service(s) were read successfully,
+ *						they can be retrieved via <i>service</i>'s <code>includedServices</code> property.
+ */
+- (void)peripheral:(CBPeripheral *)peripheral didDiscoverIncludedServicesForService:(CBService *)service error:(nullable NSError *)error {
+    
+}
+
+/**
+ *  发现外接设备特征
+ */
+- (void)peripheral:(CBPeripheral *)peripheral didDiscoverCharacteristicsForService:(CBService *)service error:(nullable NSError *)error {
+    
+    
+    if (error)
+    {
+        NSLog(@"error Discovered characteristics for %@ with error: %@", service.UUID, [error localizedDescription]);
+        return;
+    }
+    
+    for (CBCharacteristic *characteristic in service.characteristics)
+    {
+        NSLog(@"service:%@ 的 Characteristic: %@",service.UUID,characteristic.UUID);
+    }
+    
+    //获取Characteristic的值，读到数据会进入方法：-(void)peripheral:(CBPeripheral *)peripheral didUpdateValueForCharacteristic:(CBCharacteristic *)characteristic error:(NSError *)error
+    for (CBCharacteristic *characteristic in service.characteristics){
+        {
+            [peripheral readValueForCharacteristic:characteristic];
+        }
+    }
+    
+    //搜索Characteristic的Descriptors，读到数据会进入方法：-(void)peripheral:(CBPeripheral *)peripheral didDiscoverDescriptorsForCharacteristic:(CBCharacteristic *)characteristic error:(NSError *)error
+    for (CBCharacteristic *characteristic in service.characteristics){
+        [peripheral discoverDescriptorsForCharacteristic:characteristic];
+    }
+    
+    
+}
+
+/*!
+ *  @method peripheral:didUpdateValueForCharacteristic:error:
+ *
+ *  @param peripheral		The peripheral providing this information.
+ *  @param characteristic	A <code>CBCharacteristic</code> object.
+ *	@param error			If an error occurred, the cause of the failure.
+ *
+ *  @discussion				This method is invoked after a @link readValueForCharacteristic: @/link call, or upon receipt of a notification/indication.
+ */
+- (void)peripheral:(CBPeripheral *)peripheral didUpdateValueForCharacteristic:(CBCharacteristic *)characteristic error:(nullable NSError *)error {
+ 
+    //打印出characteristic的UUID和值
+    //!注意，value的类型是NSData，具体开发时，会根据外设协议制定的方式去解析数据
+    NSLog(@"characteristic uuid:%@  value:%@",characteristic.UUID,characteristic.value);
+}
+
+/*!
+ *  @method peripheral:didWriteValueForCharacteristic:error:
+ *
+ *  @param peripheral		The peripheral providing this information.
+ *  @param characteristic	A <code>CBCharacteristic</code> object.
+ *	@param error			If an error occurred, the cause of the failure.
+ *
+ *  @discussion				This method returns the result of a {@link writeValue:forCharacteristic:type:} call, when the <code>CBCharacteristicWriteWithResponse</code> type is used.
+ */
+- (void)peripheral:(CBPeripheral *)peripheral didWriteValueForCharacteristic:(CBCharacteristic *)characteristic error:(nullable NSError *)error {
+ 
+    
+}
+
+/*!
+ *  @method peripheral:didUpdateNotificationStateForCharacteristic:error:
+ *
+ *  @param peripheral		The peripheral providing this information.
+ *  @param characteristic	A <code>CBCharacteristic</code> object.
+ *	@param error			If an error occurred, the cause of the failure.
+ *
+ *  @discussion				This method returns the result of a @link setNotifyValue:forCharacteristic: @/link call.
+ */
+- (void)peripheral:(CBPeripheral *)peripheral didUpdateNotificationStateForCharacteristic:(CBCharacteristic *)characteristic error:(nullable NSError *)error {
+    
+}
+
+/*!
+ *  @method peripheral:didDiscoverDescriptorsForCharacteristic:error:
+ *
+ *  @param peripheral		The peripheral providing this information.
+ *  @param characteristic	A <code>CBCharacteristic</code> object.
+ *	@param error			If an error occurred, the cause of the failure.
+ *
+ *  @discussion				This method returns the result of a @link discoverDescriptorsForCharacteristic: @/link call. If the descriptors were read successfully,
+ *							they can be retrieved via <i>characteristic</i>'s <code>descriptors</code> property.
+ */
+- (void)peripheral:(CBPeripheral *)peripheral didDiscoverDescriptorsForCharacteristic:(CBCharacteristic *)characteristic error:(nullable NSError *)error {
+    
+    
+    //打印出Characteristic和他的Descriptors
+    NSLog(@"characteristic uuid:%@",characteristic.UUID);
+    for (CBDescriptor *d in characteristic.descriptors) {
+        NSLog(@"Descriptor uuid:%@",d.UUID);
+    }
+}
+
+/*!
+ *  @method peripheral:didUpdateValueForDescriptor:error:
+ *
+ *  @param peripheral		The peripheral providing this information.
+ *  @param descriptor		A <code>CBDescriptor</code> object.
+ *	@param error			If an error occurred, the cause of the failure.
+ *
+ *  @discussion				This method returns the result of a @link readValueForDescriptor: @/link call.
+ */
+- (void)peripheral:(CBPeripheral *)peripheral didUpdateValueForDescriptor:(CBDescriptor *) descriptor error:(nullable NSError *)error {
+
+    //打印出DescriptorsUUID 和value
+    //这个descriptor都是对于characteristic的描述，一般都是字符串，所以这里我们转换成字符串去解析
+    NSLog(@"characteristic uuid:%@  value:%@",[NSString stringWithFormat:@"%@",descriptor.UUID],descriptor.value);
+}
+
+
+- (void)peripheral:(CBPeripheral *)peripheral didWriteValueForDescriptor:(CBDescriptor *)descriptor error:(nullable NSError *)error {
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
